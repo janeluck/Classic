e77b602d195ad52c92f3e4a5fe49f564aa8328cc
janeluck <janeluck158@gmail.com>
2017-11-13 14:14:10 +0800

    init

---
+++ src/routes/Counter/components/InputButonPanel/index.js
@@ -0,0 +1,20 @@
+import {Modal} from 'antd'
+import React, {Component} from 'react'
+class A extends Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return <div>
+
+      modal
+    <Modal visible/>
+
+
+    </div>
+  }
+}
+
+
+
+export default A
